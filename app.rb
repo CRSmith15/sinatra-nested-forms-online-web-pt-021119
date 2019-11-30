66219def0497f5950c1c535f02f9126b95547a1d
Chance Smith <chancesmith97@hotmail.com>
2019-11-30 23:33:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ module FormsLab
     end
     
     get '/new' do
-      erb :'pirate/new'
+      erb :'pirates/new'
     end
     
     post '/pirates' do 
@@ -18,7 +18,7 @@ module FormsLab
         Ship.new(details)
       end
       @ships = Ship.all 
-      erb :'pirate/show'
+      erb :'pirates/show'
       
     end
 
